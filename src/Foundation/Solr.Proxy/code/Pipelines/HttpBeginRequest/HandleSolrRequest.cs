c86a308674d0e3bee8a2fb327e76cf97128042df
anton <antontishchenko@gmail.com>
2019-04-19 17:38:28 +0300

    Adding ability to use handler instead of pipelines

---
+++ src/Foundation/Solr.Proxy/code/Pipelines/HttpBeginRequest/HandleSolrRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.Pipelines.HttpRequest;
+
+namespace Foundation.SorlProxy.Pipelines.HttpBeginRequest
+{
+    public class HandleSolrRequest : HttpRequestProcessor
+    {
+        public override void Process(HttpRequestArgs args)
+        {
+            var proxy = new Proxy();
+            var argsContext = args.Context;
+            if (proxy.GetSolrResponse(ref argsContext))
+            {
+                args.AbortPipeline();
+            }
+        }
+    }
+}
---
+++ src/Foundation/Solr.Proxy/code/SolrHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Web;
+using System.Web.SessionState;
+
+namespace Foundation.SorlProxy
+{
+    public class SolrHandler : IHttpHandler, IRequiresSessionState
+    {
+        public void ProcessRequest(HttpContext context)
+        {
+            var proxy = new Proxy();
+            proxy.GetSolrResponse(ref context);
+        }
+
+        public bool IsReusable { get; }
+    }
+}
