b227d3bea55045bb1bd829a16abedc831cb16fd7
anton <antontishchenko@gmail.com>
2019-04-19 12:55:36 +0300

    Update package with configuration files

--- build/build.ps1
+++ build/build.ps1
@@ -10,12 +10,13 @@ New-Item -Name build\package -ItemType directory
 New-Item -Name build\artifacts -ItemType directory
 New-Item -Name build\package\Data -ItemType directory
 New-Item -Name build\package\bin -ItemType directory
-New-Item -Name build\package\Views -ItemType directory
+New-Item -Name build\package\solr -ItemType directory
 New-Item -Name build\package\App_Config\Include\Foundation -ItemType directory
 
-Copy-Item .\src\Foundation\SolrProxy\code\bin\Foundation.SolrProxy* .\build\package\bin
-Copy-Item .\src\Foundation\SolrProxy\code\App_Config\Include\Foundation\Foundation.SolrProxy.config .\build\package\App_Config\Include\Foundation
-Copy-Item .\src\Foundation\SolrProxy\serialization\* .\build\package\Data -recurse
+Copy-Item .\src\Foundation\Solr.Proxy\code\bin\Foundation.SolrProxy* .\build\package\bin
+Copy-Item .\src\Foundation\Solr.Proxy\code\solr\Web.config .\build\package\solr
+Copy-Item .\src\Foundation\Solr.Proxy\code\App_Config\Include\Foundation\Foundation.SolrProxy.config .\build\package\App_Config\Include\Foundation
+Copy-Item .\src\Foundation\Solr.Proxy\serialization\* .\build\package\Data -recurse
 
 $packageCmd = "Sitecore.Courier.Runner.exe -t build\package -o build\artifacts\sitecore.solrproxy." + $version + ".update -r"
 iex $packageCmd
